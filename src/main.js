ddbec163dd8d95c9228678158a2172d096cf611d
Radialarray <25952866+Radialarray@users.noreply.github.com>
2020-03-21 16:21:22 +0100

    initial commit, setup done

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
+import VueQrcodeReader from "vue-qrcode-reader";
 
 Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App)
 }).$mount("#app");
+Vue.use(VueQrcodeReader);
